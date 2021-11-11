8622d76bdfb56755ee2cd1920d43f976ec52f257
Rafael Henrique <87449612+RafaelHenrique1@users.noreply.github.com>
2021-11-11 12:20:00 -0300

    Add files via upload

---
+++ CalculadoraDoShrek.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoradoshrek;
+
+/**
+ *
+ * @author Rafael
+ */
+public class CalculadoraDoShrek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          UI_Calculadora calculadora = new UI_Calculadora();
+          calculadora.setVisible(true);
+    
+    }
+    
+    
+}
